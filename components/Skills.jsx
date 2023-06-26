4c06d4ece0b42920cee7741b54065912b13aaebf
Tasreen Mowreen <tmowreen97@gmail.com>
2023-06-26 01:13:01 -0400

    updating skills

---
+++ components/Skills.jsx
@@ -0,0 +1,21 @@
+import {BiLogoReact, BiLogoJavascript, BiLogoHtml5, BiLogoCss3, BiLogoPostgresql, BiLogoTailwindCss } from 'react-icons/bi'; 
+import {SiRubyonrails, SiNextdotjs, SiRubysinatra} from 'react-icons/si'
+import {DiRubyRough} from 'react-icons/di'
+import {BsFiletypeSql} from 'react-icons/bs'
+
+function Skills (){
+  return(
+    <div>
+      <div>
+        <div>
+          <h1>Skills</h1>CSS
+        </div>
+        <div>
+          {/* list of icons : React JS, JS, HTML, CSS, Ruby, Rubyon Rails, SQL, PostgreSQL, NextJS, Tailwind CSSSinatra */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills;
