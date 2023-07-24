ab146f96e3ea9006c577f116b14ddd7ef882844e
Tasreen Mowreen <tmowreen97@gmail.com>
2023-07-24 19:32:11 -0400

    added framer motion

--- components/ProjectList.jsx
+++ components/ProjectList.jsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
 import Image from "next/image";
+import {delay, motion} from "framer-motion";
 
 function ProjectList({image}){
   return(
     <div className="bg-cover bg-center custom-img bg-fixed bg-repeat">
     <div className="grid grid-cols-1 text-center pt-5 justify-center items-center p-2 mx-[15%]">
-    <div className="bg-white/20 md:flex items-center justify-evenly hover:scale-[1.05] rounded-xl p-2 ease-in-out duration-200">
+    <div className="bg-white/20 shadow-xl md:flex items-center justify-evenly hover:scale-[1.1] rounded-xl p-2 ease-in-out duration-200">
       <div className="pr-5 group">
-        <Image src={image.url} width='450' height='450'/>
+        <motion.div whileHover={[{ scale: 1.1 }, {rotate:-3}]} transition={{delay:.1}} className="rounded-xl overflow-hidden ">
+          <Image src={image.url} width='450' height='450'/>
+        </motion.div>
         <div className="flex relative">
         <div className="hidden group-hover:block absolute bottom-[100%] right-[50%] justify-center items-center text-xl p-2 text-[#696969] bg-white/70 shadow-gray-600/90 hover:bg-slate-600/50 hover:text-[#F9FBFD] rounded-full m-2">
           <Link href={`${image.github}`}legacyBehavior>
@@ -21,10 +24,12 @@ function ProjectList({image}){
         </div>
       </div>
       </div>
-      <div className="text-left md:max-w-[40%] mx-5">
+      <div className="text-left md:max-w-[45%] mx-5 tracking-wider">
+        <h5 className="font-extrabold">{image.title}</h5>
+        <h6 className="italic text-sm">{image.tag}</h6>
         {image.description.map((sentence)=>{
           return(
-            <p className="text-sm tracking-wider" key={sentence}>{sentence}</p>
+            <p className="text-sm" key={sentence}>{sentence}</p>
           )
         })}
       </div>
--- components/Projects_Second.jsx
+++ components/Projects_Second.jsx
@@ -3,14 +3,18 @@ import Link from "next/link";
 import ProjectList from "./ProjectList";
 function Projects_Second(){
   const projectImages = [{
+    title: "Self/Full",
+    tag: "Live a Full Life with Self-Care",
     url: "/assets/self-full-categories.png",
-    description: ["A full-stack application that promotes self care to live a fuller life style.",  "Self/Full is the one stop place for all your self-care needs. Self-care has been clinically proven to help alleviate depression and anxiety. At Self/Full you can learn more about what self-care is, how you can incorporate it in your daily life, the benefits you gain from it and learn from others also working on their self-care."],
+    description: ["An application that promotes self care to live a fuller life style.",  "Self/Full is the one stop place for all your self-care needs. Self-care has been clinically proven to help alleviate depression and anxiety. At Self/Full you can learn more about what self-care is, how you can incorporate it in your daily life, the benefits you gain from it and learn from others also working on their self-care."],
     github: "https://github.com/tmowreen97/Phase-5-Project",
     demo: "https://youtu.be/r5Gfvc3t7EI",
     stack: "React, JavaScript, HTML, CSS, Ruby, Ruby on Rails, PostgreSQL"
     //0
   }, 
   {
+    title: "Critiquer",
+    tag: "Where Everyone's a Critic",
     url: "/assets/critiquer.png",
     description: ["View and critique movies with other users.", "As a movie lover (primarily horror) I created this application where users can view a list of movies, review them and see what other users think about them as well. The list of movies is populated by the movies manually added to the database by the users. This is a great place for people to find new movies to watch and share their opinion on their favorites."],
     github: "https://github.com/tmowreen97/Phase-4-Project",
@@ -19,6 +23,8 @@ function Projects_Second(){
     //1
   }, 
   {
+    title: "MovieZone",
+    tag: "Your Ultimate Movie Destination",
     url: "/assets/movie-zone.png",
     description: ["Are you a movie buff? Enter the MovieZone!",  "Doubling down on my love for movies, in this application any user can view a list of movies that are part of the database, check out movies based on genres and finally add new movies and genres to the collection."],
     github: "https://github.com/tmowreen97/phase-3-frontend",
