100792dd7d2af75f2e864392363a0dcb6daf289c
Tasreen Mowreen <tmowreen97@gmail.com>
2023-07-28 09:46:30 -0400

    remove input box outline

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -61,7 +61,7 @@ function ContactForm({setShowThankYou}){
       <label className="block text-gray-700 text-sm font-bold mb-2">
         Email
       </label>
-      <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
       id="email" 
       name="email" 
       type="email" 
@@ -73,7 +73,7 @@ function ContactForm({setShowThankYou}){
       <label className="block text-gray-700 text-sm font-bold mb-2">
         Message
       </label>
-      <textarea className="max-h-[125px] min-h-[50px] shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+      <textarea className="max-h-[125px] min-h-[50px] shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
       id="message" 
       type="text" 
       name="message" 
