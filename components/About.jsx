c45ba0db1a36d845bcd4297bf5faf2dae3e73fa2
Tasreen Mowreen <tmowreen97@gmail.com>
2023-07-28 19:40:21 -0400

    final, still not fully mobile ready

--- components/About.jsx
+++ components/About.jsx
@@ -127,10 +127,18 @@ function About(){
 
         </div>
         </motion.div>
+        <div className='bg-cover bg-center custom-img bg-fixed text-center justify-center items-center text-xs pt-[5%] flex'>
+            <p>© Tasreen Mowreen 2023 |</p>
+            <p></p>
+            <a className="text-xs pl-1" target="_blank" href="https://media.giphy.com/media/Skv6LLzPcpxv3uEaCn/giphy.gif" rel="noopener noreferrer">
+              background img source
+            </a>
+          </div>
         </div>
         </div>
     </div>
     </div>
+    
     
   )
 }
